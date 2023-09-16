49881225759918cda891c6609399ea1d76ee2c7f
Robert Moraes <34720349+RobertMoraes@users.noreply.github.com>
2023-09-15 22:41:01 -0300

    Add files via upload

---
+++ planejando/src/main/java/br/com/projeto/bancario/planejando/PlanejandoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.bancario.planejando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanejandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanejandoApplication.class, args);
+	}
+
+}
---
+++ planejando/src/test/java/br/com/projeto/bancario/planejando/PlanejandoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.bancario.planejando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanejandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
